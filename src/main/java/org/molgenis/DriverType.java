a9ff56c4ee5f233f492d8a8f086508fc64b0cde8
AdiMac <adamsken1221@gmail.com>
2015-11-03 10:32:07 +0100

    Molgenis test local changes

--- src/main/java/org/molgenis/DriverType.java
+++ src/main/java/org/molgenis/DriverType.java
@@ -1,35 +1,42 @@
 package org.molgenis;
 
-import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 public enum DriverType implements DriverSetup
 {
-
 	FIREFOX
 	{
 		@Override
 		public DesiredCapabilities getDesiredCapabilities()
 		{
-			return DesiredCapabilities.firefox();
+			getDesiredCapabilities().setBrowserName("firefox");
+			getDesiredCapabilities().setJavascriptEnabled(true);
+//			return DesiredCapabilities.firefox();
+			return new DesiredCapabilities();
 		}
 
 		@Override
-		public WebDriver getWebDriverInstance(DesiredCapabilities capabilities)
-		{
-			FirefoxDriver driver = new FirefoxDriver(capabilities);
-			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-			return driver;
+		public RemoteWebDriver getWebDriverInstance(DesiredCapabilities capabilities) throws MalformedURLException {
+//			FirefoxDriver driver = new FirefoxDriver(capabilities);
+//			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+//			return driver;
+
+			RemoteWebDriver remoteWebDriver = new RemoteWebDriver(new URL("http://134.96.235.134:4444/wd/hub"), capabilities);
+			remoteWebDriver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+			return remoteWebDriver;
 		}
 	};
 
+
 	public static final DriverType DEFAULT_DRIVERTYPE = FIREFOX;
 
-	public WebDriver getDriver()
-	{
+	public WebDriver getDriver() throws MalformedURLException {
 		WebDriver driver = getWebDriverInstance(getDesiredCapabilities());
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		return driver;
